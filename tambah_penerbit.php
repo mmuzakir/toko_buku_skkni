52d06831342a71fbabc3220edc8a4e53af854004
Muhammad Muzakir <muhammad_muzakir@live.com>
2018-09-06 08:20:02 +0800

    tambah penerbit

---
+++ tambah_penerbit.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>Tambah Data Penerbit</title>
+</head>
+<body>
+
+	<h2>Tambah Data Penerbit</h2>
+	<a href="index.php">KEMBALI</a>
+	<br/>
+	<form method="post" action="action_tambah_Penerbit.php">
+		<table>
+			<tr>			
+				<td>Id Penerbit</td>
+				<td><input type="text" name="Id_Penerbit"></td>
+			</tr>
+			<tr>			
+				<td>Nama Penerbit</td>
+				<td><input type="text" name="Nama_Penerbit"></td>
+			</tr>
+			<tr>			
+				<td>Negara</td>
+				<td><input type="text" name="Negara"></td>
+			</tr>
+			<tr>			
+				<td>Kota</td>
+				<td><input type="text" name="Kota"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="SIMPAN"></td>
+			</tr>		
+		</table>
+	</form>
+</body>
+</html>
